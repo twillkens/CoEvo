38c864e194618057824c481b8b4514fa3088cd52
twillkens <twillkens@brandeis.edu>
2023-06-09 17:46:53 -0400

    consolidate utils

--- scratch.py
+++ scratch.py
@@ -1,3 +1,6 @@
+import os
+import csv
+import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -29,5 +32,59 @@ def plot_graph_with_edge_labels(G, edge_attr='label'):
     plt.axis("off")
     plt.show()
 
-xml = graphml_to_networkx('test.xml')
-plot_graph_with_edge_labels(xml)
+#xml = graphml_to_networkx('test.xml')
+#plot_graph_with_edge_labels(xml)
+
+
+def combine_csv_files(input_directory, output_file):
+    # Get a list of all CSV files in the directory
+    csv_files = [file for file in os.listdir(input_directory) if file.endswith(".csv")]
+
+    # Initialize an empty list to store the combined data
+    combined_data = []
+
+    # Iterate over each CSV file
+    for file in csv_files:
+        with open(os.path.join(input_directory, file), 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            next(csvreader)  # Skip header if present
+
+            # Append data rows to the combined list
+            combined_data.extend(csvreader)
+
+    # Write the combined data to a new CSV file
+    with open(output_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["left", "right", "dist"])  # Write header
+        csvwriter.writerows(combined_data)
+
+# Usage example
+# combine_csv_files("/path/to/input/directory", "/path/to/output/file.csv")
+
+def analyze_data(csv_file = "fsm_dist.csv"):
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv(csv_file)
+
+    # Compute summary statistics
+    min_dist = df['dist'].min()
+    max_dist = df['dist'].max()
+    mean_dist = df['dist'].mean()
+    median_dist = df['dist'].median()
+    std_dist = df['dist'].std()
+
+    # Print the summary statistics
+    print("Summary Statistics:")
+    print("Min Distance:", min_dist)
+    print("Max Distance:", max_dist)
+    print("Mean Distance:", mean_dist)
+    print("Median Distance:", median_dist)
+    print("Standard Deviation:", std_dist)
+
+    # Plot a histogram of the "dist" column
+    plt.hist(df['dist'], bins=10)
+    plt.xlabel('Distance')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Distance')
+    plt.show()
+
+# Usage example
