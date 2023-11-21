f86c88c2fb1c6a8ba36d41827c64cd1bbfeb47f4
twillkens <twillkens@brandeis.edu>
2023-11-20 20:16:51 -0500

    archive directory fix

--- run/modes_toolbox.jl
+++ run/modes_toolbox.jl
@@ -5,14 +5,20 @@ using Distributed
 
 function create_modes_reports(;
     trials::Vector{Int} = collect(1:20),
+    archive_directory::String = "/media/tcw/Seagate/two_comp/",
     perform_parallel::Bool = true,
     kwargs...
 )
     # complete me
     if perform_parallel
-        reports = pmap(trial -> ModesTrialReport(trial = trial; kwargs...), trials)
+        reports = pmap(trial -> ModesTrialReport(
+            trial = trial, archive_directory = archive_directory; kwargs...), trials
+        )
     else
-        reports = [ModesTrialReport(trial = trial; kwargs...) for trial in trials]
+        reports = [
+            ModesTrialReport(trial = trial, archive_directory = archive_directory; kwargs...) 
+            for trial in trials
+        ]
     end
     return reports
 end
